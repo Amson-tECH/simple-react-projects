294c31e6365664687e3926bea152392e64235bb9
Reuben Korsi Amuzu <reubenamuzu23@gmail.com>
2025-10-04 22:00:19 +0000

    Done with P04

--- 04-react-calculator/src/App.jsx
+++ 04-react-calculator/src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Calcualtor from './Components/Calcualtor'
+import Calculator from './Components/Calculator'
 
 const App = () => {
   return (
     <div>
-      <Calcualtor />
+      <Calculator />
     </div>
   )
 }
